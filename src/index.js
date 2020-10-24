73796b4047d9806891cdb27af1714c5c37790488
antipea <71250351+antipea@users.noreply.github.com>
2020-10-24 23:05:43 +0300

    feat: add common function check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    if (str.length % 2 === 1) {
+        return false;
+    }
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+                arr.push(str[i]);
+                break;
+            }
+            if (str[i] === bracketsConfig[j][1] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+                if (arr[arr.length - 1] === bracketsConfig[j][0]) {
+                    arr.pop();
+                    break;
+                }
+            }
+            if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+                if (arr[arr.length - 1] !== str[i]) {
+                    arr.push(str[i]);
+                    break;
+                } else {
+                    arr.pop();
+                }
+            }
+        }
+    }
+
+    return arr.length === 0 ? true : false;
 }
